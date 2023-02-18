8b366d5dfda0d76497fbe04768746c96fe713d45
张朝阳 <jeespring@163.com>
2023-02-18 23:26:50 +0800

    Spring Cloud Eureka

---
+++ spring-cloud-eureka1/src/test/java/com/jeespring/eureka/Eureka1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeespring.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eureka1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
